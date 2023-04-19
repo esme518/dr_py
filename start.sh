74d75caf558973e4da641d49a4c174cf468a2984
hjdhnx <hjd124579>
2023-04-19 22:58:51 +0800

    修正文档错误

--- start.sh
+++ start.sh
@@ -2,13 +2,10 @@
 # 建立文件夹
 mkdir -p /home/pywork/dr_py && cd /home/pywork/dr_py
 # 下载仓库包
-wget https://code.gitlink.org.cn/api/v1/repos/hjdhnx/dr_py/archive/master/dr_py-master.tar.gz
+wget https://code.gitlink.org.cn/api/v1/repos/hjdhnx/dr_py/archive/master.zip
 # 解压
-tar -xzvf dr*.tar.gz
-# 移动文件到当前目录
-mv dr*/* ./
+unzip master.zip
 # 清理临时文件
-rm -r dr_py-master
-rm *.tar.gz
+rm *.zip
 # 启动容器
 # docker run -it -p 5705:5705 -p 9001:9001 -v /home/pywork/dr_py:/root/sd/pywork/dr_py --restart=always --name drpy -d hjdhnx/drpy
